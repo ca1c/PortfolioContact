9953c0fb956ed7ed1d24961042eb241ba4963d13
ca1c <gegs921@gmail.com>
2022-06-24 21:03:41 -0700

    headers

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
-app.use(cors({
-    origin: 'https://portfolio-ca1c.vercel.app/'
-}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://portfolio-ca1c.vercel.app/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
