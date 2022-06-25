d83627159f9917cac88c52cbba715494792c5c56
ca1c <gegs921@gmail.com>
2022-06-24 21:09:17 -0700

    typo in origin

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://portfolio-ca1c.vercel.app/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://portfolio-ca1c.vercel.app"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
